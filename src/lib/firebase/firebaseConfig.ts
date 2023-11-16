e5faf048b48642b60805eee88bec9b4589acf137
Leonardo Contreras <joseleoc123@gmail.com>
2023-11-15 23:50:06 -0400

    eas config

--- src/lib/firebase/firebaseConfig.ts
+++ src/lib/firebase/firebaseConfig.ts
@@ -1,23 +1,23 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import {
-  REACT_APP_FB_apiKey,
-  REACT_APP_FB_authDomain,
-  REACT_APP_FB_projectId,
-  REACT_APP_FB_storageBucket,
-  REACT_APP_FB_messagingSenderId,
-  REACT_APP_FB_appId,
-  REACT_APP_FB_measurementId,
+  EXPO_PUBLIC_FB_apiKey,
+  EXPO_PUBLIC_FB_authDomain,
+  EXPO_PUBLIC_FB_projectId,
+  EXPO_PUBLIC_FB_storageBucket,
+  EXPO_PUBLIC_FB_messagingSenderId,
+  EXPO_PUBLIC_FB_appId,
+  EXPO_PUBLIC_FB_measurementId,
 } from "@env";
 
 export const firebaseConfig = {
-  apiKey: REACT_APP_FB_apiKey,
-  authDomain: REACT_APP_FB_authDomain,
-  projectId: REACT_APP_FB_projectId,
-  storageBucket: REACT_APP_FB_storageBucket,
-  messagingSenderId: REACT_APP_FB_messagingSenderId,
-  appId: REACT_APP_FB_appId,
-  measurementId: REACT_APP_FB_measurementId,
+  apiKey: EXPO_PUBLIC_FB_apiKey,
+  authDomain: EXPO_PUBLIC_FB_authDomain,
+  projectId: EXPO_PUBLIC_FB_projectId,
+  storageBucket: EXPO_PUBLIC_FB_storageBucket,
+  messagingSenderId: EXPO_PUBLIC_FB_messagingSenderId,
+  appId: EXPO_PUBLIC_FB_appId,
+  measurementId: EXPO_PUBLIC_FB_measurementId,
 };
 
 const firebase = initializeApp(firebaseConfig);
